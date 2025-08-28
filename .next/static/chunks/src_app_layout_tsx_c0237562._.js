a552826a500b3ff9f4aa96a5229e6c2e2ef1db14
liehan123 <78044738+liehan123@users.noreply.github.com>
2025-08-28 15:42:56 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1756370573627

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_@supabase_node-fetch_browser_eb6c6189.js",
+  "static/chunks/node_modules_next_dist_6d0b70ed._.js",
+  "static/chunks/node_modules_@supabase_auth-js_dist_module_6004bf20._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_2814f315._.js",
+  "static/chunks/src_efae71cf._.js"
+],
+    source: "dynamic"
+});
